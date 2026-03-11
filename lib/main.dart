4038db0f66fb2e9b708073f1a60dfab940bc4c7e
Muhammad Agus Solehuddin <2306033@itg.ac.id>
2026-03-11 19:38:13 +0700

    Membuat Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,183 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(HomePage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    String title = 'Aplikasi Pertamaku';
-    String name = 'Muhammad Agus';
-    String message = 'Hallo nama saya $name, selamat datang di $title';
-
+class HomePage extends StatelessWidget{
+  build(context){
     return MaterialApp(
-      title: title,
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.deepOrange,
-          title: Text(title),
+          backgroundColor: Colors.amberAccent,
+          leading: Icon(Icons.home),
+          title: Text('Aplikasi Pertamaku'),
         ),
-        body: Column(
-          children: [
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Padding(padding: EdgeInsets.all(8.0),
-            child: Text(message))
-          ]
+        body: SingleChildScrollView(
+          child:Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            children:[
+              Row(
+                children: [
+                  Icon(Icons.archive),
+                  Text("artikel terbaru", style: TextStyle(fontWeight: .bold),)
+                ],
+              ),
+              Card(child: Column(
+                crossAxisAlignment: .start,
+                children: [
+                  Image.network(
+                    'https://picsum.photos/id/237/600/300'
+                  ),
+                  Text("Artikel Tentang Anjing", style: TextStyle(fontWeight: .bold)),
+                  Text("adalah keturunan serigala yang telah didomestikasi. Disebut juga sebagai anjing domestik, hewan ini dibiakkan secara selektif selama kala Pleistosen Akhir oleh para pemburu-pengumpul. Anjing dan serigala abu-abu modern berbagi nenek moyang yang sama.[4] Anjing merupakan spesies pertama yang didomestikasi lebih dari 14.000 tahun yang lalu, sebelum adanya perkembangan pertanian, meskipun studi genetika menunjukkan bahwa proses domestikasi mungkin telah dimulai lebih dari 25.000 tahun silam. Berkat hubungan panjangnya dengan manusia, anjing telah memperoleh kemampuan untuk bertahan hidup dengan pola makan kaya pati yang sebenarnya tidak memadai bagi jenis Canidae lainnya.")
+                ],
+              ),
+              ),
+              SizedBox(height: 20,),
+              Row(
+                children: [
+                  Icon(Icons.comment),
+                  Text(
+                    "Komentar",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: .bold
+                    ),
+                  ),
+                ],
+              ),
+              ListView(
+                shrinkWrap: true,
+                children: [
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text('Hafiz'),
+                          Text('kukira anjing sama srigala beda, ternyata masih satu keturunan ya'),
+                        ]
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text('Jek'),
+                          Text('Satu keturunan, tapi lebih elegan srigala sih'),
+                        ]
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text('Acil'),
+                          Text('Artikel ini memberikan informasi yang menarik tentang asal-usul anjing sebagai hewan yang telah lama hidup berdampingan dengan manusia'),
+                        ]
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text('Pasun'),
+                          Text('Penjelasan dalam artikel ini membuat saya lebih memahami sejarah domestikasi anjing sejak ribuan tahun yang lalu'),
+                        ]
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text('Gufron'),
+                          Text('Saya bisa berkomunikasi dengannya'),
+                        ]
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text('Santri Gufron'),
+                          Text('Lucu tapi takut karna najis'),
+                        ]
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text('Dani jon'),
+                          Text('Aku udah punya 3 pelihara anjing'),
+                        ]
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text('cipp'),
+                          Text('hoammmm'),
+                        ]
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text('faizz'),
+                          Text('Sieun ku anjing mah'),
+                        ]
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text('pet'),
+                          Text('Anjing FF sama ga'),
+                        ]
+                      ),
+                    ),
+                  ),
+                ]
+              )
+            ],
+          ),
         ),
-    ),
-    );
-  }
-
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
+        )
         
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
